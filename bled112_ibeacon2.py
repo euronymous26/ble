5fb3efff00141070bacd8407e6e646cd9a3664bf
euronymous26 <mireles.rodriguez.enrique@gmail.com>
2017-01-14 18:54:55 -0700

    Update bled112_ibeacon2.py

--- bled112_ibeacon2.py
+++ bled112_ibeacon2.py
@@ -83,6 +83,107 @@ class MyParser(optparse.OptionParser):
         # Drop the last "\n", or the header if no options or option groups:
         return "".join(result[:-1])
 
+# define API commands we might use for this script
+def ble_cmd_system_reset(p, boot_in_dfu):
+    p.write(struct.pack('5B', 0, 1, 0, 0, boot_in_dfu))
+def ble_cmd_connection_disconnect(p, connection):
+    p.write(struct.pack('5B', 0, 1, 3, 0, connection))
+def ble_cmd_gap_set_mode(p, discover, connect):
+    p.write(struct.pack('6B', 0, 2, 6, 1, discover, connect))
+def ble_cmd_gap_end_procedure(p):
+    p.write(struct.pack('4B', 0, 0, 6, 4))
+def ble_cmd_gap_set_adv_parameters(p, adv_interval_min, adv_interval_max, adv_channels):
+    p.write(struct.pack('<4BHHB', 0, 5, 6, 8, adv_interval_min, adv_interval_max, adv_channels))
+def ble_cmd_gap_set_adv_data(p, set_scanrsp, adv_data):
+    p.write(struct.pack('<4BBB' + str(len(adv_data)) + 's', 0, 2 + len(adv_data), 6, 9, set_scanrsp, len(adv_data), b''.join(chr(i) for i in adv_data)))
+
+# define basic BGAPI parser
+bgapi_rx_buffer = []
+bgapi_rx_expected_length = 0
+def bgapi_parse(b):
+    global bgapi_rx_buffer, bgapi_rx_expected_length, options
+    if len(bgapi_rx_buffer) == 0 and (b == 0x00 or b == 0x80):
+        bgapi_rx_buffer.append(b)
+    elif len(bgapi_rx_buffer) == 1:
+        bgapi_rx_buffer.append(b)
+        bgapi_rx_expected_length = 4 + (bgapi_rx_buffer[0] & 0x07) + bgapi_rx_buffer[1]
+    elif len(bgapi_rx_buffer) > 1:
+        bgapi_rx_buffer.append(b)
+
+    #print '%02X: %d, %d' % (b, len(bgapi_rx_buffer), bgapi_rx_expected_length)
+    if bgapi_rx_expected_length > 0 and len(bgapi_rx_buffer) == bgapi_rx_expected_length:
+        #print '<=[ ' + ' '.join(['%02X' % b for b in bgapi_rx_buffer ]) + ' ]'
+        packet_type, payload_length, packet_class, packet_command = bgapi_rx_buffer[:4]
+        bgapi_rx_payload = b''.join(chr(i) for i in bgapi_rx_buffer[4:])
+        if packet_type & 0x80 == 0x00: # response
+            bgapi_filler = 0
+        else: # event
+            if packet_class == 0x06: # gap
+                if packet_command == 0x00: # scan_response
+                    rssi, packet_type, sender, address_type, bond, data_len = struct.unpack('<bB6sBBB', bgapi_rx_payload[:11])
+                    sender = [ord(b) for b in sender]
+                    data_data = [ord(b) for b in bgapi_rx_payload[11:]]
+                    display = 1
+
+                    # parse all ad fields from ad packet
+                    ad_fields = []
+                    this_field = []
+                    ad_flags = 0
+                    ad_services = []
+                    ad_local_name = []
+                    ad_tx_power_level = 0
+                    ad_manufacturer = []
+
+                    bytes_left = 0
+                    for b in data_data:
+                        if bytes_left == 0:
+                            bytes_left = b
+                            this_field = []
+                        else:
+                            this_field.append(b)
+                            bytes_left = bytes_left - 1
+                            if bytes_left == 0:
+                                ad_fields.append(this_field)
+                                if this_field[0] == 0x01: # flags
+                                    ad_flags = this_field[1]
+                                if this_field[0] == 0x02 or this_field[0] == 0x03: # partial or complete list of 16-bit UUIDs
+                                    for i in xrange((len(this_field) - 1) / 2):
+                                        ad_services.append(this_field[-1 - i*2 : -3 - i*2 : -1])
+                                if this_field[0] == 0x04 or this_field[0] == 0x05: # partial or complete list of 32-bit UUIDs
+                                    for i in xrange((len(this_field) - 1) / 4):
+                                        ad_services.append(this_field[-1 - i*4 : -5 - i*4 : -1])
+                                if this_field[0] == 0x06 or this_field[0] == 0x07: # partial or complete list of 128-bit UUIDs
+                                    for i in xrange((len(this_field) - 1) / 16):
+                                        ad_services.append(this_field[-1 - i*16 : -17 - i*16 : -1])
+                                if this_field[0] == 0x08 or this_field[0] == 0x09: # shortened or complete local name
+                                    ad_local_name = this_field[1:]
+                                if this_field[0] == 0x0A: # TX power level
+                                    ad_tx_power_level = this_field[1]
+
+                                # OTHER AD PACKET TYPES NOT HANDLED YET
+
+                                if this_field[0] == 0xFF: # manufactuerer specific data
+                                    ad_manufacturer.append(this_field[1:])
+
+                    #print "gap_scan_response: rssi: %d, packet_type: %d, sender: %s, address_type: %d, bond: %d, data_len: %d" % \
+                    #    (rssi, packet_type, ':'.join(['%02X' % ord(b) for b in sender[::-1]]), address_type, bond, data_len)
+                    t = datetime.datetime.now()
+
+                    disp_list = []
+                    disp_list.append("%ld.%03ld" % (time.mktime(t.timetuple()), t.microsecond/1000))
+                    disp_list.append("%d" % rssi)
+                    disp_list.append("%s" % ''.join(['%02X' % b for b in sender[::-1]]))
+                    disp_list.append("%d" % address_type)
+
+                    print ' '.join(disp_list)
+
+        bgapi_rx_buffer = []
+
+# gracefully exit without a big exception message if possible
+def ctrl_c_handler(signal, frame):
+    #print 'Goodbye, cruel world!'
+    exit(0)
+
 def main():
     global options
 
@@ -300,106 +401,6 @@ Scan request output example with public address 00:07:80:81:44:94 and RSSI -57:
             # don't burden the CPU
             time.sleep(0.01)
 
-# define API commands we might use for this script
-def ble_cmd_system_reset(p, boot_in_dfu):
-    p.write(struct.pack('5B', 0, 1, 0, 0, boot_in_dfu))
-def ble_cmd_connection_disconnect(p, connection):
-    p.write(struct.pack('5B', 0, 1, 3, 0, connection))
-def ble_cmd_gap_set_mode(p, discover, connect):
-    p.write(struct.pack('6B', 0, 2, 6, 1, discover, connect))
-def ble_cmd_gap_end_procedure(p):
-    p.write(struct.pack('4B', 0, 0, 6, 4))
-def ble_cmd_gap_set_adv_parameters(p, adv_interval_min, adv_interval_max, adv_channels):
-    p.write(struct.pack('<4BHHB', 0, 5, 6, 8, adv_interval_min, adv_interval_max, adv_channels))
-def ble_cmd_gap_set_adv_data(p, set_scanrsp, adv_data):
-    p.write(struct.pack('<4BBB' + str(len(adv_data)) + 's', 0, 2 + len(adv_data), 6, 9, set_scanrsp, len(adv_data), b''.join(chr(i) for i in adv_data)))
-
-# define basic BGAPI parser
-bgapi_rx_buffer = []
-bgapi_rx_expected_length = 0
-def bgapi_parse(b):
-    global bgapi_rx_buffer, bgapi_rx_expected_length, options
-    if len(bgapi_rx_buffer) == 0 and (b == 0x00 or b == 0x80):
-        bgapi_rx_buffer.append(b)
-    elif len(bgapi_rx_buffer) == 1:
-        bgapi_rx_buffer.append(b)
-        bgapi_rx_expected_length = 4 + (bgapi_rx_buffer[0] & 0x07) + bgapi_rx_buffer[1]
-    elif len(bgapi_rx_buffer) > 1:
-        bgapi_rx_buffer.append(b)
-
-    #print '%02X: %d, %d' % (b, len(bgapi_rx_buffer), bgapi_rx_expected_length)
-    if bgapi_rx_expected_length > 0 and len(bgapi_rx_buffer) == bgapi_rx_expected_length:
-        #print '<=[ ' + ' '.join(['%02X' % b for b in bgapi_rx_buffer ]) + ' ]'
-        packet_type, payload_length, packet_class, packet_command = bgapi_rx_buffer[:4]
-        bgapi_rx_payload = b''.join(chr(i) for i in bgapi_rx_buffer[4:])
-        if packet_type & 0x80 == 0x00: # response
-            bgapi_filler = 0
-        else: # event
-            if packet_class == 0x06: # gap
-                if packet_command == 0x00: # scan_response
-                    rssi, packet_type, sender, address_type, bond, data_len = struct.unpack('<bB6sBBB', bgapi_rx_payload[:11])
-                    sender = [ord(b) for b in sender]
-                    data_data = [ord(b) for b in bgapi_rx_payload[11:]]
-                    display = 1
-
-                    # parse all ad fields from ad packet
-                    ad_fields = []
-                    this_field = []
-                    ad_flags = 0
-                    ad_services = []
-                    ad_local_name = []
-                    ad_tx_power_level = 0
-                    ad_manufacturer = []
-
-                    bytes_left = 0
-                    for b in data_data:
-                        if bytes_left == 0:
-                            bytes_left = b
-                            this_field = []
-                        else:
-                            this_field.append(b)
-                            bytes_left = bytes_left - 1
-                            if bytes_left == 0:
-                                ad_fields.append(this_field)
-                                if this_field[0] == 0x01: # flags
-                                    ad_flags = this_field[1]
-                                if this_field[0] == 0x02 or this_field[0] == 0x03: # partial or complete list of 16-bit UUIDs
-                                    for i in xrange((len(this_field) - 1) / 2):
-                                        ad_services.append(this_field[-1 - i*2 : -3 - i*2 : -1])
-                                if this_field[0] == 0x04 or this_field[0] == 0x05: # partial or complete list of 32-bit UUIDs
-                                    for i in xrange((len(this_field) - 1) / 4):
-                                        ad_services.append(this_field[-1 - i*4 : -5 - i*4 : -1])
-                                if this_field[0] == 0x06 or this_field[0] == 0x07: # partial or complete list of 128-bit UUIDs
-                                    for i in xrange((len(this_field) - 1) / 16):
-                                        ad_services.append(this_field[-1 - i*16 : -17 - i*16 : -1])
-                                if this_field[0] == 0x08 or this_field[0] == 0x09: # shortened or complete local name
-                                    ad_local_name = this_field[1:]
-                                if this_field[0] == 0x0A: # TX power level
-                                    ad_tx_power_level = this_field[1]
-
-                                # OTHER AD PACKET TYPES NOT HANDLED YET
-
-                                if this_field[0] == 0xFF: # manufactuerer specific data
-                                    ad_manufacturer.append(this_field[1:])
-
-                    #print "gap_scan_response: rssi: %d, packet_type: %d, sender: %s, address_type: %d, bond: %d, data_len: %d" % \
-                    #    (rssi, packet_type, ':'.join(['%02X' % ord(b) for b in sender[::-1]]), address_type, bond, data_len)
-                    t = datetime.datetime.now()
-
-                    disp_list = []
-                    disp_list.append("%ld.%03ld" % (time.mktime(t.timetuple()), t.microsecond/1000))
-                    disp_list.append("%d" % rssi)
-                    disp_list.append("%s" % ''.join(['%02X' % b for b in sender[::-1]]))
-                    disp_list.append("%d" % address_type)
-
-                    print ' '.join(disp_list)
-
-        bgapi_rx_buffer = []
-
-# gracefully exit without a big exception message if possible
-def ctrl_c_handler(signal, frame):
-    #print 'Goodbye, cruel world!'
-    exit(0)
 
 signal.signal(signal.SIGINT, ctrl_c_handler)
 
